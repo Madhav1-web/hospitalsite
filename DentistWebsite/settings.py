b1834e20dbbed27e96b002188f385e620b754ffa
Madhav Mohanakrishnan <madhavm2002@gmail.com>
2021-03-07 15:49:10 +0530

    heroku new tweak

--- DentistWebsite/settings.py
+++ DentistWebsite/settings.py
@@ -122,7 +122,7 @@ EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'madhavm2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'lioness1234rarai'
+EMAIL_HOST_PASSWORD = 'buyerdayrarai1234'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
